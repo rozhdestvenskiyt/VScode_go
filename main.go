aa1f5305403708c31630f9e084bbdb60f5b07eff
rozhdestvenskiyt <timurrozdestvenskij98@gmail.ru>
2025-12-19 16:25:51 +0500

    i added logger in my project

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-
+	"fmt"
 	"itprogergolang/internal/user"
+	"itprogergolang/pkg/logging"
 	"log"
 	"net"
 	"time"
@@ -14,24 +15,29 @@ import (
 
 
 func main() {
+	logger := logging.GetLogger()
+	logger.Info("Проекта начался")
 	router := httprouter.New()
 	handler := user.Handler_user{}
 	handler.Register(router)
 
-	start_server(router)
+	start_server(router , &logger)
 }
 
-func start_server(router *httprouter.Router) {
+func start_server(router *httprouter.Router , logger *logging.Logger) {
 	listener , err := net.Listen("tcp" , ":8080")
 	if err != nil {
 		log.Fatal("Error with listener")
 	}
+	logger.Info("Запустили listener")
+
 
 	server := &http.Server{
 		Handler: router,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout: 15 * time.Second,
 	}
-
+	
 	log.Fatal(server.Serve(listener))
+	fmt.Println("Project died")
 }
--- pkg/logging/loggin.go
+++ pkg/logging/loggin.go
@@ -1,1 +1,84 @@
 package logging
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+)
+
+var e *logrus.Entry
+
+type Logger struct {
+	*logrus.Entry
+}
+
+func GetLogger() Logger {
+	return Logger{e}
+}
+
+type MyHook struct {
+	Writers []io.Writer
+	LogLevels []logrus.Level
+}
+
+
+func (h *MyHook) Fire(entry *logrus.Entry) error {
+	line , err := entry.String()
+	if err != nil {
+		log.Fatal("error")
+	}
+
+	for _ , w := range h.Writers {
+		w.Write([]byte(line))
+	}
+
+	return nil
+}
+
+func (h *MyHook) Levels() []logrus.Level {
+	return h.LogLevels
+}
+
+func init() {
+	logger := logrus.New()
+
+	logger.SetReportCaller(true)
+
+	logger.Formatter = &logrus.TextFormatter{
+		CallerPrettyfier: func(f *runtime.Frame) (function string, file string) {
+			filename := filepath.Base(f.File)
+			return f.Function, fmt.Sprintf("%s:%d", filename, f.Line)
+		},
+
+		FullTimestamp: true,
+		DisableColors: false,
+	}
+
+	err := os.MkdirAll("logs" , 0755)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	file , err := os.OpenFile("logs/all.log" , os.O_CREATE|os.O_WRONLY|os.O_APPEND , 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+
+	logger.AddHook(&MyHook{
+		Writers: []io.Writer{file , os.Stdout},
+		LogLevels: logrus.AllLevels,
+	})
+
+	logger.SetOutput(io.Discard)
+	logger.SetLevel(logrus.TraceLevel)
+	
+	
+	
+	e = logrus.NewEntry(logger)
+}
