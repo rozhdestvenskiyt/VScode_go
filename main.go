dde1ef8e6e4a77402c85930059e7b8c066839072
rozhdestvenskiyt <timurrozdestvenskij98@gmail.ru>
2025-12-19 13:58:58 +0500

    I created folder and changed main.go

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+
+func add(num_1 int , num_2 int) int {
+	result := num_1 * num_2
+	return result
+}
+
 func main() {
-	fmt.Println("Hello , World!")
+	fmt.Printf("The result is %d" , add(5 ,5))
 }
